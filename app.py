306afd1db6dc218e203e2fc65743847f27042583
gustxdel <115872141+gustxdel@users.noreply.github.com>
2022-12-11 12:09:53 +0100

    added html layouts
    home and layout html and css added. Added route home to app

--- app.py
+++ app.py
@@ -1,2 +1,20 @@
-from flask import flask
+import os
 
+from flask import Flask, flash, jsonify, redirect, render_template, request, session
+from cs50 import SQL
+from flask_session import Session
+from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
+from werkzeug.security import check_password_hash, generate_password_hash
+
+
+app = Flask(__name__)
+
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)

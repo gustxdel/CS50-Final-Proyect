57c63007ea2d90ca9e862b109627234f51dbdcfa
gustxdel <115872141+gustxdel@users.noreply.github.com>
2022-12-17 07:19:38 +0100

    added account route
    added account route
    added flash to login and reguster

--- app.py
+++ app.py
@@ -20,22 +20,33 @@ db = SQL("sqlite:///budgetark.db")
 def home():
     return render_template("home.html")
 
+@app.route("/account", methods=["GET", "POST"])
+def account():
+    return render_template("account.html")
+
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
     form=RegistrationForm()
     if form.validate_on_submit():
+        flash(f"Account created succesfully", category="success")
         return redirect("/login")
     return render_template("register.html",form=form)
    
 
-
 @app.route("/login", methods=["GET", "POST"])
 def login():
     form=LoginForm()
-    
+    if form.validate_on_submit():
+        if form.username.data=="gusifer" and form.password.data=="12345678":
+            flash(f"Login succesfully", category="success")
+            return redirect("/account")
+        else:
+            flash(f"Login unsuccesfully", category="danger")    
     return render_template("login.html",form=form)
-    
+
+
+
     
 if __name__ == "__main__":
     app.run(debug=True)

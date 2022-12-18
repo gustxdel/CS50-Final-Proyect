800382905161b113099105896d27dddeacf65713
gustxdel <115872141+gustxdel@users.noreply.github.com>
2022-12-18 13:42:18 +0100

    bugs fixed

--- final/__init__.py
+++ final/__init__.py
@@ -1,12 +1,18 @@
 from flask import Flask
 from cs50 import SQL
+from flask_session import Session
+from tempfile import mkdtemp
 
-from final.models import initialize_db
+
 app=Flask(__name__)
 
 app.config["SECRET_KEY"]="thisiscs50project"
+app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
 
-db= SQL("sqlite:///database/budgetark.db")
+db= SQL("sqlite:///final/database/budgetark.db")
    
 from final import routes
--- final/routes.py
+++ final/routes.py
@@ -18,13 +18,19 @@ def account():
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
+    session.clear()
     form=RegistrationForm()
     if form.validate_on_submit():
         username= form.username.data
         password= form.password.data
         confirm= form.confirm_password.data
-        hashcode = generate_password_hash(password, method='pbkdf2:sha256', salt_length=8)
-        db.execute("INSERT INTO users (username, password) VALUES(?, ?)", username, hashcode)
+        rows = db.execute("SELECT * FROM users WHERE username = ?", username)
+        if len(rows) == 1:
+            flash(f"Username already in use", category="danger")
+            return render_template("register.html",form=form)
+        else:
+            hashcode = generate_password_hash(password, method='pbkdf2:sha256', salt_length=8)
+            db.execute("INSERT INTO users (username, password) VALUES(?, ?)", username, hashcode)
         
         flash(f"Account created succesfully", category="success")
         return redirect("/login")
@@ -33,15 +39,35 @@ def register():
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
+    session.clear()
     form=LoginForm()
     if form.validate_on_submit():
-        if form.username.data=="gusifer" and form.password.data=="12345678":
-            flash(f"Login succesfully", category="success")
-            return redirect("/account")
-        else:
-            flash(f"Login unsuccesfully", category="danger")    
-    return render_template("login.html",form=form)
+        if request.method == "POST":
+            username= form.username.data
+            password= form.password.data
+            rows = db.execute("SELECT * FROM users WHERE username = ?", username)
+            hash= check_password_hash(rows[0]["password"])
+
+            if username =="gusifer":
+                flash(f"Login unsuccesfully. Incorrect Username or Password", category="danger")    
+                return redirect("/login")
+            else:
+                session["user_id"] = rows[0]["id"]
+                flash(f"Login succesfully", category="success")
+                return redirect("/account")
+    else:    
+        return render_template("login.html",form=form)
+
+            
 
 
+@app.route("/logout")
+def logout():
+    """Log user out"""
 
+    # Forget any user_id
+    session.clear()
+
+    # Redirect user to login form
+    return redirect("/")
    

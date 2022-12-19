70b6d799c183860bd2d47ac966f153bd63a0fc0b
gustxdel <115872141+gustxdel@users.noreply.github.com>
2022-12-19 14:01:39 +0100

    added forms for budget and expenses

--- final/forms.py
+++ final/forms.py
@@ -1,7 +1,7 @@
 import os
 
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField, FloatField
 from wtforms.validators import DataRequired, Length, EqualTo
 
 class RegistrationForm(FlaskForm):
@@ -14,3 +14,12 @@ class LoginForm(FlaskForm):
     username = StringField(label='Username',validators=[DataRequired(),Length(min=3,max=20)])
     password = PasswordField(label='Password',validators=[DataRequired(),Length(min=8,max=16)])
     submit = SubmitField(label='Log In')
+
+class BudgetForm(FlaskForm):
+    set_budget = FloatField(label='Budget', validators=[DataRequired()])    
+    submit = SubmitField(label='Set Budget')
+
+class ExpensesForm(FlaskForm):
+    name = StringField(label='Name',validators=[DataRequired()])
+    expense = FloatField(label='Expense', validators=[DataRequired()])
+    submit = SubmitField(label='Submit')    
